15def422347d8660dbba1df62f74b3f066d0dff7
TimaVol <tima.voloshuk@gmail.com>
2022-01-05 02:13:27 +0200

    add auth system (local, jwt)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CrudModule } from './crud/crud.module';
-
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(), CrudModule],
+  imports: [TypeOrmModule.forRoot(), CrudModule, AuthModule],
   controllers: [],
   providers: [],
 })
--- src/crud/crud.module.ts
+++ src/crud/crud.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { CrudController } from './crud.controller';
 import { CrudService } from './crud.service';
 import { Work } from './work.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Work])],
+  imports: [TypeOrmModule.forFeature([Work]), AuthModule],
   controllers: [CrudController],
   providers: [CrudService],
 })
