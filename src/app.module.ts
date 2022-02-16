70e9198c74a70925162f6006be8b66a467a91e9e
TimaVol <tima.voloshuk@gmail.com>
2022-02-17 00:10:52 +0200

    add env configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CrudModule } from './crud/crud.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [TypeOrmModule.forRoot(), CrudModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        `${process.cwd()}/config/.env.development`,
+        `${process.cwd()}/config/.env.prodaction`,
+      ],
+    }),
+    TypeOrmModule.forRoot(),
+    CrudModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
